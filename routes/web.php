426969057641fc4308bc8e6e3d9153fcdea28cc2
vagrant <yangke687@gmail.com>
2018-06-02 02:52:26 +0000

    Add 'test' funcationality route

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('test', function(){
+	
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
