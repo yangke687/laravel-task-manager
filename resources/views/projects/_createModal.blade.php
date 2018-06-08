6ee0550f7c7601e54a61799737993bea7412ae70
vagrant <yangke687@gmail.com>
2018-06-08 13:59:56 +0000

    Create resource routes for 'projects'

--- resources/views/projects/_createModal.blade.php
+++ resources/views/projects/_createModal.blade.php
@@ -13,7 +13,7 @@
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
-      {!!Form::open()!!}
+      {!!Form::open(['route' => 'projects.store', 'method' => 'POST', 'files' => true])!!}
       <div class="modal-body">
           <div class="form-group">
             {!!Form::label('name','Project Name', ['class' => 'control-label'])!!}
--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::resource('projects', 'ProjectsController');
