106e967ccd14e87fc321f28367f241f95db89238
vagrant <yangke687@gmail.com>
2018-06-24 02:41:08 +0000

    Add 'intervention/image' plugin package

--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Collective\Html\HtmlServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
 
     ],
 
@@ -212,7 +213,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
-
+        'Image' => Intervention\Image\Facades\Image::class,
     ],
 
 ];
